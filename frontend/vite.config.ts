ac8521e1930d57b7f54bbe59c7b6ed553bea3902
muhibsakin4880 <chronofi.preseed@gmail.com>
2026-03-13 06:00:53 +0600

    Update frontend config for Redoubt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    esbuild: {
+        logOverride: { 
+            'this-is-undefined-in-esm': 'silent' 
+        }
+    },
     server: {
         host: '0.0.0.0', // Listen on all network interfaces
         port: 5173,
